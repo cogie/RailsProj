6303c8d6d40cabca757f3736f339715c5689553e
Kojiee <mac7barcela@gmail.com>
2024-01-01 19:27:19 +0800

    Complete a create functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,32 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  #instantiate new article but does not save it
+  def new
+    @article = Article.new
+  end
+
+  # instantiate new articles with values for the title and body, and attempts to save it, 
+  # if article is save redirects to http://localhost:3000/articles/#{@article.id}, else returns 422
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  #filters parameters
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
+
+#notes
+#redirect_to will cause the browser to make a new request, whereas render renders the specified view for the current request.
+#It is important to use redirect_to after mutating the database or application state.
+#Otherwise, if the user refreshes the page, the browser will make the same request, and the mutation will be repeated.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+    #The first validation declares that a title value must be present. Because title is a string, 
+    #this means that the title value must contain at least one non-whitespace character.
+    validates :title, presence: true
+
+    #The second validation declares that a body value must also be present. Additionally,
+    #it declares that the body value must be at least 10 characters long.
+    validates :body, presence: true, length: {minimum: 10}
 end
