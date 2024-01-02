fc254eec45a765788c0df6204784f7fed5b6300f
Kojiee <mac7barcela@gmail.com>
2024-01-02 11:58:15 +0800

    complete edit and update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,14 +26,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  #edit and update an existing data and stores it in @article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   #filters parameters
   private
     def article_params
       params.require(:article).permit(:title, :body)
     end
 end
-
-#notes
-#redirect_to will cause the browser to make a new request, whereas render renders the specified view for the current request.
-#It is important to use redirect_to after mutating the database or application state.
-#Otherwise, if the user refreshes the page, the browser will make the same request, and the mutation will be repeated.
